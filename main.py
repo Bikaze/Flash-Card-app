f6c833d4e0af1c6eb212730f94952478279610f9
bkz <clmntmugisha@gmail.com>
2024-01-01 16:08:49 +0200

    updated main.py

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ from tkinter import *
 import pandas
 from random import choice
 
+tim = ''
 frame = pandas.read_csv('./data/french_words.csv')
 words = frame.to_dict(orient='records')
 
 
+def reset():
+    canvas.itemconfig(canvas_image, image=front)
+    canvas.itemconfig(title, fill='black')
+    canvas.itemconfig(word, fill='black')
+
+
+def flip_card(dic):
+    canvas.itemconfig(canvas_image, image=back)
+    canvas.itemconfig(title, text='English', fill='white')
+    canvas.itemconfig(word, text=dic['English'], fill='white')
+
+
 def gen_word():
+    reset()
+    global tim
     """Generates a word to display on the canvas"""
     new_key = choice(words)
-    title['text'] = 'French'
-    word['text'] = new_key['French']
+    canvas.itemconfig(title, text='French')
+    canvas.itemconfig(word, text=new_key['French'])
+    tim = window.after(3000, flip_card, new_key)
 
 
 BACKGROUND_COLOR = "#B1DDC6"
@@ -21,14 +37,11 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front = PhotoImage(file='./images/card_front.png')
-canvas.create_image(400, 263, image=front)
+back = PhotoImage(file='./images/card_back.png')
+canvas_image = canvas.create_image(400, 263, image=front)
+title = canvas.create_text(400, 190, text='', font=('Arial', 40, 'italic'))
+word = canvas.create_text(400, 283, text='', font=('Arial', 60, 'bold'))
 canvas.grid(column=0, row=0, columnspan=2)
-
-title = Label(text='', font=('Arial', 40, 'italic'), bg='white')
-title.place(x=330, y=120)
-
-word = Label(text='', font=('Arial', 60, 'bold'), bg='white')
-word.place(x=290, y=223)
 
 wrong = PhotoImage(file='./images/wrong.png')
 w_bt = Button(image=wrong, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=gen_word)
